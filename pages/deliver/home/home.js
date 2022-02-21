f53ae70431351d16c49e0243738b8eaf265350b8
lincy <lincy@wecut.com>
2022-02-21 19:05:58 +0800

    增加页面

---
+++ pages/deliver/home/home.js
@@ -0,0 +1,44 @@
+// pages/deliver/home/home.js
+Page({
+  data: {
+    cardCur: 0,
+    swiperList: [{
+      id: 0,
+      type: 'image',
+      url: 'https://ossweb-img.qq.com/images/lol/web201310/skin/big84000.jpg'
+    }, {
+      id: 1,
+      type: 'image',
+      url: 'https://ossweb-img.qq.com/images/lol/web201310/skin/big84001.jpg',
+    }, {
+      id: 2,
+      type: 'image',
+      url: 'https://ossweb-img.qq.com/images/lol/web201310/skin/big39000.jpg'
+    }, {
+      id: 3,
+      type: 'image',
+      url: 'https://ossweb-img.qq.com/images/lol/web201310/skin/big10001.jpg'
+    }, {
+      id: 4,
+      type: 'image',
+      url: 'https://ossweb-img.qq.com/images/lol/web201310/skin/big25011.jpg'
+    }, {
+      id: 5,
+      type: 'image',
+      url: 'https://ossweb-img.qq.com/images/lol/web201310/skin/big21016.jpg'
+    }, {
+      id: 6,
+      type: 'image',
+      url: 'https://ossweb-img.qq.com/images/lol/web201310/skin/big99008.jpg'
+    }],
+  },
+  getCurrentIndex: function (e) {
+    this.setData({
+      cardCur: e.detail.current
+    })
+  },
+  onClick: function (e) {
+    console.log(this.data.swiperList[this.data.cardCur])
+  },
+
+})

7eff0a632c13a2f60e052248e1138ffddce48886
lincy <lincy@wecut.com>
2022-02-22 18:49:54 +0800

    fix

--- pages/deliver/home/home.js
+++ pages/deliver/home/home.js
@@ -5,31 +5,7 @@ Page({
     swiperList: [{
       id: 0,
       type: 'image',
-      url: 'https://ossweb-img.qq.com/images/lol/web201310/skin/big84000.jpg'
-    }, {
-      id: 1,
-      type: 'image',
-      url: 'https://ossweb-img.qq.com/images/lol/web201310/skin/big84001.jpg',
-    }, {
-      id: 2,
-      type: 'image',
-      url: 'https://ossweb-img.qq.com/images/lol/web201310/skin/big39000.jpg'
-    }, {
-      id: 3,
-      type: 'image',
-      url: 'https://ossweb-img.qq.com/images/lol/web201310/skin/big10001.jpg'
-    }, {
-      id: 4,
-      type: 'image',
-      url: 'https://ossweb-img.qq.com/images/lol/web201310/skin/big25011.jpg'
-    }, {
-      id: 5,
-      type: 'image',
-      url: 'https://ossweb-img.qq.com/images/lol/web201310/skin/big21016.jpg'
-    }, {
-      id: 6,
-      type: 'image',
-      url: 'https://ossweb-img.qq.com/images/lol/web201310/skin/big99008.jpg'
+      url: '../../../images/kd.png'
     }],
   },
   getCurrentIndex: function (e) {
@@ -40,5 +16,10 @@ Page({
   onClick: function (e) {
     console.log(this.data.swiperList[this.data.cardCur])
   },
-
+  goToDelever: function (e) {
+    console.log(e)
+    wx.navigateTo({
+      url: '/pages/deliver/index/deliver',
+    })
+  }
 })

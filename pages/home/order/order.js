6fcbb167585775a0bb60130f4152078f066b3768
atsukodan <>
2022-02-23 22:43:04 +0800

    运单列表

--- pages/home/order/order.js
+++ pages/home/order/order.js
@@ -1,11 +1,33 @@
 // pages/home/order/order.js
+// 获取应用实例
+const app = getApp()
+
 Page({
   data: {
-
+    orderList: []
   },
   goToDetail: function (e) {
     wx.navigateTo({
       url: '/pages/home/orderDetail/orderDetail',
     })
-  }
+  },
+  onLoad() {
+    this.getOrders()
+  },
+  getOrders() {
+    var that = this;
+    wx.request({
+      url: 'https://money.atsukodan.cn/web/orders',
+      header: {
+        "headerSignature": app.globalData.signature
+      },
+      method: "GET",
+      success(res) {
+        console.log(res);
+        that.setData({
+          orderList: res.data.data,
+        });
+      }
+    })
+  },
 })
